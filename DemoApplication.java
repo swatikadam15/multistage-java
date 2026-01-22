9ed9b16bc4d6a17c1f6d7ce91fe770fb731a99b5
Swati Kadam <46994107+swatikadam15@users.noreply.github.com>
2026-01-22 07:20:17 +0000

    Delete DemoApplication.java

--- DemoApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-public class DemoApplication {
-
-    public static void main(String[] args) {
-        System.out.println("Hello from Java App running inside Docker!");
-    }
-}
-
